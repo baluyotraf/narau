063192ee4bc6b974f029258172ff1d42c5b8fb5b
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-08-23 10:30:42 +0800

    Added setup file for the library

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='iv-model',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    install_requires=[
+    ],
+)
