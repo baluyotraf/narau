fdd61c84114e4d7d1ac4a92cdc4d920fc2440878
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-09-20 15:15:38 +0800

    Added initial model for 2 label sentiment analysis

--- narau/estimators.py
+++ narau/estimators.py
@@ -57,3 +57,65 @@ class SiameseBiLSTMEmbedding(tf.estimator.Estimator):
                 return tf.estimator.EstimatorSpec(mode, predictions={'embedding': x_})
 
         super().__init__(model_fn, model_dir, config, None, warm_start_from)
+
+
+class BinarySentimentClassifier(tf.estimator.Estimator):
+
+    def __init__(self, embedding_size, embedding_dims,
+                 embedding_special_tokens, embedding_with_pad,
+                 embedding_weights, embedding_trainable,
+                 embedding_units,
+                 lstm_units, lstm_drop_out,
+                 projection_units,
+                 learning_rate=0.1, model_dir=None,
+                 config=None, warm_start_from=None):
+
+        def model_fn(features, labels, mode):
+            emb = layers.TokenEmbedding(embedding_size, embedding_dims, tf.float32,
+                                        embedding_special_tokens, embedding_with_pad,
+                                        embedding_trainable)
+            embt = layers.EmbeddingTransform(embedding_units, tf.nn.relu)
+            lstm = layers.BidirectionalLSTM(lstm_units,
+                                            drop_out=(lstm_drop_out if mode == tf.estimator.ModeKeys.TRAIN
+                                                      else None))
+            proj = layers.Projection(projection_units, tf.nn.relu)
+
+            x, len_ = features['x'], features['len']
+            x = emb(x)
+            x = embt(x)
+            x = lstm(x, len_)
+            x = proj(x)
+
+            if mode == tf.estimator.ModeKeys.PREDICT:
+                with tf.variable_scope('predictions'):
+                    logit = tf.squeeze(x, axis=1)
+                    probability = tf.nn.sigmoid(logit)
+                    label = tf.cast(probability + 0.5, tf.int32)
+
+                predictions = {
+                    'logit': logit,
+                    'probability': probability,
+                    'label': label,
+                }
+
+                return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+            with tf.variable_scope('loss'):
+                loss = tf.losses.sigmoid_cross_entropy(labels, x)
+
+            if mode == tf.estimator.ModeKeys.EVAL:
+                return tf.estimator.EstimatorSpec(mode, loss=loss)
+
+            with tf.variable_scope('train'):
+                optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+                train_op = optimizer.minimize(loss, tf.train.get_global_step())
+
+            def scaffold_init(scaffold, session):
+                emb.feed_dict_init(session, embedding_weights)
+            scaffold = tf.train.Scaffold(init_fn=scaffold_init)
+
+            return tf.estimator.EstimatorSpec(mode, loss=loss,
+                                              train_op=train_op,
+                                              scaffold=scaffold)
+
+        super().__init__(model_fn, model_dir, config, None, warm_start_from)
