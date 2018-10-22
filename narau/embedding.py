4219c060756face3be0a4bbf0ba7b8262a7caafe
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-10-22 14:20:52 +0800

    Added doc strings for the embedding module

--- narau/embedding.py
+++ narau/embedding.py
@@ -2,6 +2,12 @@ import numpy as np
 
 
 def load_glove_weights(path, encoding='utf-8'):
+    """
+    Loads the weights from a glove formatted file
+    :param path: path to the glove formatted file
+    :param encoding: file encoding
+    :return: numpy array containing the embeddings from the file
+    """
     with open(path, 'r', encoding=encoding) as file:
         weights = [[float(v) for v in line.strip().split(' ')[1:]] for line in file]
         weights = np.asarray(weights)
